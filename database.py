78490f6a3174a857e719e1b7c5f4c4deda4fabc4
Chris Jakins <cjakins12@yahoo.com>
2018-11-25 12:00:10 -0600

    Add all table creation queries (only if tables do not already exist). Remove 'name' parameter from database constructor

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import queries
 
 class Database:
     
-    def __init__(self, name):
+    def __init__(self):
         # create a connection to the database
         self.conn = sqlite3.connect('headnovel.db')
 
@@ -11,7 +11,10 @@ class Database:
     def init_tables(self):
         c = self.conn.cursor()
         c.execute(queries.create_profile_query)
-        # add all create table statements here
-        # then uncomment below lines
+        c.execute(queries.create_post_query)
+        c.execute(queries.create_comment_query)
+        c.execute(queries.create_like_query)
+        c.execute(queries.create_message_query)
+        c.execute(queries.create_page_query)
         self.conn.commit()
         self.conn.close()
--- queries.py
+++ queries.py
@@ -1,6 +1,6 @@
 # write all queries here
 
-create_profile_query = ('CREATE TABLE profile ('
+create_profile_query = ('CREATE TABLE IF NOT EXISTS profile ('
     'f_name varchar(20) NOT NULL,'
     'l_name varchar(20) NOT NULL,'
     'u_name varchar(20) NOT NULL,'
@@ -12,4 +12,53 @@ create_profile_query = ('CREATE TABLE profile ('
     'profile_pic varchar(256),'
     'b_date date NOT NULL,'
     'friends_list int,'
-    'PRIMARY KEY(profile_id) );')
+    'PRIMARY KEY(profile_id),'
+    'UNIQUE(email) );')
+
+
+create_post_query = ('CREATE TABLE IF NOT EXISTS POST('
+    'post_id int NOT NULL,'
+    'time_stamp timestamp,'
+    'poster_id int NOT NULL,'
+    'likes_list int,'
+    'PRIMARY KEY(post_id) );')
+
+
+create_comment_query = ('CREATE TABLE IF NOT EXISTS COMMENT('
+    'comment_id int NOT NULL,'
+    'time_stamp timestamp NOT NULL,'
+    'comm_text varchar(256) NOT NULL,'
+    'commentor_ID int NOT NULL,'
+    'likes_list int,'
+    'PRIMARY KEY(comment_id) );')
+
+
+create_like_query = ('CREATE TABLE IF NOT EXISTS `LIKE`('
+    'like_id int NOT NULL,'
+    'time_stamp timestamp NOT NULL,'
+    'liker_id int NOT NULL,'
+    'PRIMARY KEY(like_id) );')
+
+
+create_message_query = ('CREATE TABLE IF NOT EXISTS MESSAGE('
+    'sender_id int NOT NULL,'
+    'receiver_id int NOT NULL,'
+    'time_stamp timestamp NOT NULL,'
+    'message_text varchar(256) NOT NULL,'
+    'likes_list int,'
+    'PRIMARY KEY(sender_id,receiver_id,time_stamp) );')
+
+
+create_page_query = ('CREATE TABLE IF NOT EXISTS PAGE ('
+    'page_id int NOT NULL,'
+    'page_name varchar(128) NOT NULL,'
+    'admin int NOT NULL,'
+    'page_image varchar(256),'
+    'num_views int,'
+    'category varchar(64),'
+    'description varchar(256),'
+    'member_list int,'
+    'PRIMARY KEY (page_id),'
+    'FOREIGN KEY (admin) REFERENCES PROFILE(profile_id) );')
+
+
