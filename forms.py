e54adfef2b1928e093f3a4bc60ab39aaddd66d84
Chris Jakins <cjakins12@yahoo.com>
2018-11-25 13:38:05 -0600

    Change timestamp fields to datetime for sqlite. flask.Form is deprecated, so update to Flask.FlaskForm

--- forms.py
+++ forms.py
@@ -1,8 +1,11 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 import wtforms
+import datetime
 
-class PostForm(Form):
+class PostForm(FlaskForm):
     post_text = wtforms.TextAreaField('Post')
     username = wtforms.TextField('Username')
+    current_time = datetime.datetime.now()
+    print(current_time)
 
     submit = wtforms.SubmitField('Submit')
