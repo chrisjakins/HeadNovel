15693004c1c0ea3c73d43209fc46fb2db13077fc
luisG97 <gonzalez.mex7@gmail.com>
2018-11-26 15:10:15 -0600

    here is a commit with what i have so far. It's not a lot, but it's something. message and profile added.

--- client.py
+++ client.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request, flash, session, escape, redirect, url_for
-from forms import PostForm, CommentForm
+from forms import PostForm, CommentForm, MessageForm, ProfileForm
 
 debug = True
 app = Flask(__name__)
 app.secret_key = 'dev key'
-
+###############################################################
 @app.route('/')
 def index():
     if 'username' in session:
         return 'Logged in as %s' % escape(session['username'])
     return 'You are not logged in'
+
+###############################################################
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -23,11 +25,15 @@ def login():
         </form>
     '''
 
+###############################################################
+
 @app.route('/logout')
 def logout():
     # removes username is someone is logged in
     session.pop('username', None)
     return redirect(url_for('index'))
+
+###############################################################
 
 @app.route('/comment', methods = ['GET', 'POST'])
 def comment():
@@ -42,6 +48,8 @@ def comment():
     elif request.method == 'GET':
         return render_template('comment.html', form = form)
 
+###############################################################
+
 @app.route('/post', methods = ['GET', 'POST'])
 def post():
     form = PostForm()
@@ -55,6 +63,37 @@ def post():
     elif request.method == 'GET':
         return render_template('post.html', form = form)
 
+###############################################################
+
+@app.route('/message', methods = ['GET', 'POST'])
+def message():
+    form = MessageForm()
+    if request.method == 'POST':
+        if form.validate() is False:
+            flash('Check required fields.')
+            return render_template('message.html', form = form)
+        else:
+            # add results to database here
+            return render_template('success.html')
+    elif request.method == 'GET':
+        return render_template('message.html', form = form)
+
+###############################################################
+
+@app.route('/profile', methods = ['GET', 'POST'])
+def profile():
+    form = ProfileForm()
+    if request.method == 'POST':
+        if form.validate() is False:
+            flash('Check required fields.')
+            return render_template('profile.html', form = form)
+        else:
+            # add results to database here
+            return render_template('success.html')
+    elif request.method == 'GET':
+        return render_template('profile.html', form = form)
+
+###############################################################
 
 if __name__ == '__main__':
     app.run(debug = debug)
--- forms.py
+++ forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 import wtforms
 import datetime
 
+###############################################################################
+
 class PostForm(FlaskForm):
     # TODO 
     # need a unique post ID (can be abstracted to database class)
@@ -15,6 +17,8 @@ class PostForm(FlaskForm):
 
     submit = wtforms.SubmitField('Submit')
 
+###############################################################################
+
 class CommentForm(FlaskForm):
     # TODO
     # unique ID to post commented on
@@ -26,4 +30,48 @@ class CommentForm(FlaskForm):
 
     current_time = datetime.datetime.now()
 
+    submit = wtforms.SubmitField('Submit')
+
+###############################################################################
+
+class MessageForm(FlaskForm):
+    # TODO
+    # need a unique post ID
+    message_text = wtforms.TextAreaField('Message',
+        [wtforms.validators.DataRequired('Message field can not be empty.'),
+         wtforms.validators.Length(max = 400)])
+ 
+    receiver_username = wtforms.TextField('Receiver Username',
+        [wtforms.validators.DataRequired('Receiver Username field can not be empty.')])
+ 
+    current_time = datetime.datetime.now()
+
+    submit = wtforms.SubmitField('Submit')
+
+###############################################################################
+
+class ProfileForm(FlaskForm):                                                     
+    # TODO                                                                     
+    # need a unique post ID (can be abstracted to database class)              
+    password_text = wtforms.TextAreaField('Password',                                  
+        [wtforms.validators.DataRequired('Password field can not be empty.'),      
+         wtforms.validators.Length(max = 400)])                                
+                                                                               
+    username = wtforms.TextField('Username',                                   
+        [wtforms.validators.DataRequired('Username field can not be empty.')])
+
+    email = wtforms.TextField('Email',                                   
+        [wtforms.validators.DataRequired('Email field can not be empty.')])
+ 
+    firstName = wtforms.TextField('First Name',                                   
+        [wtforms.validators.DataRequired('First Name field can not be empty.')])
+
+    lastName = wtforms.TextField('Last Name',                                   
+        [wtforms.validators.DataRequired('Last Name field can not be empty.')])
+
+    phoneNum = wtforms.TextField('Phone Number',                                   
+        [wtforms.validators.DataRequired('Phone Number field can not be empty.')])
+
+    current_time = datetime.datetime.now()                                     
+ 
     submit = wtforms.SubmitField('Submit')
